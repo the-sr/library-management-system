ff3faf14c88130e06afe20fd5835c1589d0ebe72
the-sr <thesagarrijal@gmail.com>
2024-05-06 19:50:58 +0545

    UserBook Service updated

--- api/app/controller/user.book.controller.js
+++ api/app/controller/user.book.controller.js
@@ -1,6 +1,26 @@
+const BookService = require("../services/book.services");
+const book_service = new BookService();
+
+const UserBookService = require("../services/user.book.service");
+const user_book_service = new UserBookService();
+
+
 class UserBookController {
 
-    borrowBook = (req, res, next) => {
+    borrowBook = async (req, res, next) => {
+        let data = req.body;
+        let book = await book_service.getBookByTitle(data);
+        if (book) {
+            //user detais : fetch from req and forward to UserBookModel
+            //book details: fetch and forward to UserBookModel
+            res.json({
+                result: book,
+                status: true,
+                msg: "Book Borrowed Successfully"
+            })
+        } else {
+            next({ status: 404, msg: "Book Not Found" });
+        }
 
     }
 
